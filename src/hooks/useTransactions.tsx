21977a7d741cf31e068d2c0634bac916b9d80b0a
Ederson <ederson@trilhante.com.br>
2022-03-04 16:16:46 -0300

    migrating context to hooks

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import { createContext, useEffect, useState, ReactNode } from 'react';
-import { api } from './services/api';
+import { createContext, useEffect, useState, useContext, ReactNode } from 'react';
+import { api } from '../services/api';
 
 interface Transaction{
     id: number;
@@ -32,7 +32,7 @@ interface TransactionsContextData{
 
 type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
 
-export const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData);
+const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData);
 
 export function TransactionsProvider({children}: TransactionsProviderProps){
     const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -58,4 +58,10 @@ export function TransactionsProvider({children}: TransactionsProviderProps){
             {children}
         </TransactionsContext.Provider>
     );
+}
+
+export function useTransactions() {
+    const context = useContext(TransactionsContext);
+
+    return context;
 }
