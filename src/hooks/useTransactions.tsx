2fc0c71eddc9214283030d7e6ad940a8dfa1cdf4
EdersonRodrigues7 <ederson@trilhante.com.br>
2022-06-27 13:13:02 -0300

    refactor: consuming custom api

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,67 +1,55 @@
 import { createContext, useEffect, useState, useContext, ReactNode } from 'react';
 import { api } from '../services/api';
 
-interface Transaction{
-    id: number;
-    title: string;
-    type: string;
-    category: string;
-    amount: number;
-    createdAt: string
+interface Transaction {
+  id: number;
+  title: string;
+  type: string;
+  category: string;
+  amount: number;
+  createdAt: string;
 }
 
-interface TransactionsProviderProps{
-    children: ReactNode;
+interface TransactionsProviderProps {
+  children: ReactNode;
 }
 
-interface TransactionsContextData{
-    transactions: Transaction[];
-    createTransaction: (transaction: TransactionInput) => Promise<void>;
+interface TransactionsContextData {
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
-
-// DIFERENTES FORMAS DE TIPAR AS INFOS QUE O USUÁRIO COLOCA NO FORM (TransactionInput)
-
-// interface TransactionInput{
-//     title: string;
-//     type: 'deposit' | 'withdraw';
-//     category: string;
-//     amount: number;
-// }
-
-// type TransactionInput = Pick<Transaction, 'title' | 'amount' | 'type' | 'category'>;
 
 type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
 
 const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData);
 
-export function TransactionsProvider({children}: TransactionsProviderProps){
-    const [transactions, setTransactions] = useState<Transaction[]>([]);
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-    useEffect(() => {
-        api.get('transactions')
-        .then(response => setTransactions(response.data.transactions));
-    }, []);
+  useEffect(() => {
+    api.get('http://localhost:3332/transactions').then(response => setTransactions(response.data));
+  }, []);
 
-    async function createTransaction(transactionInput: TransactionInput){
-        const response = await api.post('/transactions', {
-            ...transactionInput,
-            createdAt: new Date(),
-        });
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post('http://localhost:3332/transactions', {
+      ...transactionInput,
+      createdAt: new Date().toISOString()
+    });
 
-        const { transaction } = response.data;
+    const transaction = response.data;
 
-        setTransactions([...transactions, transaction]);
-    }
+    setTransactions([...transactions, transaction]);
+  }
 
-    return (
-        <TransactionsContext.Provider value={{transactions, createTransaction}}>
-            {children}
-        </TransactionsContext.Provider>
-    );
+  return (
+    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
+      {children}
+    </TransactionsContext.Provider>
+  );
 }
 
 export function useTransactions() {
-    const context = useContext(TransactionsContext);
+  const context = useContext(TransactionsContext);
 
-    return context;
+  return context;
 }
