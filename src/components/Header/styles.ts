61f9adad731fa1dc16045811af41da71887b0ba2
EdersonRodrigues7 <ederson@trilhante.com.br>
2022-06-27 13:16:34 -0300

    style: header breakpoint

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,29 +1,35 @@
 import styled from 'styled-components';
 
 export const Container = styled.header`
-    background: var(--blue);
-`
+  background: var(--blue);
+`;
 
 export const Content = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    max-width: 1120px;
-    margin: 0 auto;
-    padding: 2rem 1rem 12rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 2rem 1rem 12rem;
 
-    button {
-        font-size: 1rem;
-        color: var(--shape);
-        background: var(--blue-light);
-        border: 0;
-        border-radius: 0.25rem;
-        padding: 0 2rem;
-        height: 3rem;
-        transition: filter 0.2s;
+  button {
+    font-size: 1rem;
+    color: var(--shape);
+    background: var(--blue-light);
+    border: 0;
+    border-radius: 0.25rem;
+    padding: 0 2rem;
+    height: 3rem;
+    transition: filter 0.2s;
 
-        &:hover{
-            filter: brightness(0.9);
-        }
+    &:hover {
+      filter: brightness(0.9);
     }
-`
+  }
+  @media (max-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    align-items: center;
+  }
+`;
